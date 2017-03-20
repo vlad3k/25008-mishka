d6299bf732c84c2c1a94ebe7b057e9c49c269623
Vlad Poezzhaev <uzumake-1996@mail.ru>
2017-03-20 12:45:37 +0500

    Отступы

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var iconMenu = document.querySelector('.page-header__toggle');
-var menu        = document.querySelector('.page-nav');
-
-iconMenu.addEventListener('click', openMenu);
-
-function openMenu() {
-	menu.classList.toggle('page-nav--open');
-}
