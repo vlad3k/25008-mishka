fa9a3d3aa7b624b5f4ba6f0d52a4457b87436c6a
Vlad Poezzhaev <uzumake-1996@mail.ru>
2017-04-03 09:58:43 +0500

    Ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,17 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var run = require("run-sequence");
+var del = require("del");
+var cheerio = require("gulp-cheerio");
+var	replace = require("gulp-replace");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -13,22 +23,88 @@ gulp.task("style", function() {
     .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
-        "last 2 versions"
-      ]})
+        "last 2 version",
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("build/img/icons/*.svg")
+  .pipe(cheerio({
+			run: function ($) {
+				$("[fill]").removeAttr("fill");
+				$("[stroke]").removeAttr("stroke");
+				$("[style]").removeAttr("style");
+			},
+			parserOptions: {xmlMode: true}
+		}))
+    .pipe(replace("&gt;", ">"))
+    .pipe(svgmin())
+    .pipe(svgstore({
+       inlineSvg: true
+     }))
+    .pipe(rename("symbols.svg"))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build"
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", ["html:update"]);
 });
+
+gulp.task("html:copy", function() {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("html:update", ["html:copy"], function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task("copy", function() {
+ return gulp.src([
+ "fonts/**/*.{woff,woff2}",
+ "img/**",
+ "js/**",
+ "*.html"
+ ], {
+ base: "."
+ })
+ .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+ return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean",
+  "style",
+   "copy",
+   "images",
+   "symbols",
+    fn
+  );
+})
